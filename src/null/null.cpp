00e943d6019907c0cf674191f66114cf1addc6a2
silentboy302 <0day20hour@gmail.com>
2017-04-06 07:09:08 +0600

    Added null pattern code

---
+++ src/null/null.cpp
@@ -0,0 +1,77 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+//Forward declaration of object
+class FileInfo;
+//An interface for serializer
+//this way we can have multiple serializer
+class Serializer
+{
+public:
+    string tostring(int s){
+        stringstream ss;
+        string ret;
+        ss << s;
+        ss >> ret;
+        return ret;
+    }
+    virtual string serialize(FileInfo *fi) = 0;
+};
+//A structure to hold file information
+class FileInfo
+{
+public:
+    string name, parent_directory;
+    int size;
+    FileInfo(string n, int p, int s){
+        name = n;
+        parent_directory = p;
+        size = s;
+    }
+    string serialize(Serializer *sz){
+        cout << sz->serialize(this) << endl;
+    }
+};
+
+//Now, pretend we do not yet decided how to serialize our data
+//Then, how we can proceed so that the code does not break?
+//Yes, There must some default implementation of this class
+//This is called Null object design pattern
+
+//We code a default behavior of Serialize, most of the time it
+//will be no operation. But as long long as the 'default behavior semantic
+//holds, any implementation is fine.
+
+class NullSerializer: public Serializer
+{
+public:
+    string serialize(FileInfo *fi){
+        string res = "";
+        res += fi->name + "@";
+        res += fi->parent_directory+"$";
+        res += tostring(fi->size);
+
+        return res;
+    }
+};
+
+//Of course, There will be not null serializer
+class JSONSerializer: public Serializer
+{
+public:
+    string serialize(FileInfo *fi){
+        string res = "";
+        res += "{ name: "+fi->name+", ";
+        res += "parent: "+fi->parent_directory+", ";
+        res += "size: "+tostring(fi->size)+" }";
+
+        return res;
+    }
+};
+
+int main()
+{
+    FileInfo file("")
+}
