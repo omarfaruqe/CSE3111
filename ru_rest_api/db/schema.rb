908bf4c08a1cf24f67d395f2d8833ede00058748
Golam Azam <golamazam6228ru@gmail.com>
2017-05-16 12:21:14 +0600

    Hall management is working

--- ru_rest_api/db/schema.rb
+++ ru_rest_api/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511080934) do
+ActiveRecord::Schema.define(version: 20170516060611) do
+
+  create_table "halls", force: :cascade do |t|
+    t.string "name"
+    t.string "seat"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "infos", force: :cascade do |t|
     t.string "date"
