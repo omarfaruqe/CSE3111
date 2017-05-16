ae2e9123a479fbc8211905aa54fc65e409d8af90
Anamulrucse <rony.cseru@gmail.com>
2017-05-16 11:09:32 +0600

    Student_information api

--- ru_rest_api/db/schema.rb
+++ ru_rest_api/db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511080934) do
+ActiveRecord::Schema.define(version: 20170516045115) do
 
   create_table "infos", force: :cascade do |t|
     t.string "date"
     t.string "length"
     t.string "time"
     t.string "roll"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "Student_id"
+    t.string "Name"
+    t.string "Department"
+    t.string "Session"
+    t.string "eligible_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
