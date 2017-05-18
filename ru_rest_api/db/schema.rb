9a09f914aead23236a12a556b766cfaa11d84dee
RezaDx <nasim.reza.ru.cse15@gmail.com>
2017-05-18 11:45:05 +0600

    it's working well for operation GET,PUT,POST,DELETE. The table name is student

--- ru_rest_api/db/schema.rb
+++ ru_rest_api/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511080934) do
+ActiveRecord::Schema.define(version: 20170516055948) do
 
-  create_table "infos", force: :cascade do |t|
-    t.string "date"
-    t.string "length"
-    t.string "time"
-    t.string "roll"
+  create_table "students", force: :cascade do |t|
+    t.string "Name"
+    t.string "Institute_Name"
+    t.string "Email"
+    t.string "Phone_No"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
