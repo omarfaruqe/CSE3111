62e3d791cc72994e268b42332648e226d837f6ca
mine is: Ashiqur rahman <ashiqurcseru@gmail.com>
2017-07-24 03:21:28 +0600

    Fixing models unit test

--- ru_rest_api/spec/models/residence_spec.rb
+++ ru_rest_api/spec/models/residence_spec.rb
@@ -1,7 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Residence, type: :model do
-  it { should validate_presence_of(:Dormitory_name) }
-  it { should validate_presence_of(:Total_seat) }
-  it { should validate_presence_of(:Notice) }
+ before :each do
+	@item =  Residence.new
+  end
+
+
+  it "is valid with valid attributes" do
+  @item.Dormitory_name = "anything" 
+  @item.Total_seat = "anything" 
+  @item.Notice = "anything" 
+ # @item.year = "anything" 
+  expect(@item).to be_valid
+  end
+
+  it "is invalid without a Dormitory_name" do
+  	@item.Dormitory_name = nil
+  	expect(@item).to_not be_valid
+  end	
+
+it "is invalid without a Total_seat" do
+  	@item.Total_seat = nil
+  	expect(@item).to_not be_valid
+  end	
+
+  it "is invalid without a Notice" do
+  	@item.Notice = nil
+  	expect(@item).to_not be_valid
+  end	
+
+  #it "is invalid without a year" do
+  #	@item.year = nil
+  	#expect(@item).to_not be_valid
+ # end	
+
 end
