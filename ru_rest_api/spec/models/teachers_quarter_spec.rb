7cf0d3ea924493a7f55b66edf4aca004d85d0f68
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2017-07-24 03:00:40 +0600

    some test case added

--- ru_rest_api/spec/models/teachers_quarter_spec.rb
+++ ru_rest_api/spec/models/teachers_quarter_spec.rb
@@ -1,7 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe TeachersQuarter, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:block) }
-  it { should validate_presence_of(:location) }
+  # it { should validate_presence_of(:name) }
+  # it { should validate_presence_of(:block) }
+  # it { should validate_presence_of(:location) }
+  # 
+  
+  before :each do
+  	@teachers_quarter = TeachersQuarter.new
+  end
+
+  it 'should validate with valid attributes' do
+  	@teachers_quarter.name = 'foobar'
+  	@teachers_quarter.block = 'foobar'
+  	@teachers_quarter.location = 'foobar'
+  	expect(@teachers_quarter).to be_valid
+  end
+
+  it 'should not be valid without name' do
+  	@teachers_quarter.name = nil
+  	expect(@teachers_quarter).to_not be_valid
+  end
+
+  it 'should not be valid without block' do
+  	@teachers_quarter.block = nil
+  	expect(@teachers_quarter).to_not be_valid
+  end
+
+  it 'should not be valid without location' do
+  	@teachers_quarter.location = nil
+  	expect(@teachers_quarter).to_not be_valid
+  end
+
+
 end
--- ru_rest_api/spec/models/user_spec.rb
+++ ru_rest_api/spec/models/user_spec.rb
@@ -6,7 +6,34 @@ RSpec.describe User, type: :model do
   #it { should have_many(:teachers_quarter) }
   # Validation tests
   # ensure name, email and password_digest are present before save
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
+  # it { should validate_presence_of(:name) }
+  # it { should validate_presence_of(:email) }
+  # it { should validate_presence_of(:password_digest) }
+  # 
+  
+  before :each do
+  	@user = User.new
+  end
+
+  it 'should validate with valid attributes' do
+  	@user.name = 'noName'
+  	@user.email = 'ex@mail.com'
+  	@user.password = 'password'
+  	expect(@user).to be_valid
+  end
+
+  it 'should not be valid without user name' do 
+  	@user.name = nil
+  	expect(@user).to_not be_valid
+  end
+
+  it 'should not be valid without email' do
+  	@user.email = nil
+  	expect(@user).to_not be_valid
+  end
+
+  it 'should not be valid without password' do
+  	@user.password = nil
+  	expect(@user).to_not be_valid
+  end
 end
